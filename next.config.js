d9fdcc566ff7c0633252eaf0f0b07d3e4300499b
Nathan Wassink <nathan.wassink@gmail.com>
2024-01-23 00:00:31 +0100

    test

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = ''
 let basePath = ''
 
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
