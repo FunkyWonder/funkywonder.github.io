112134c2c09e7b362bca1332625b820f8f027968
Nathan Wassink <nathan.wassink@gmail.com>
2024-01-22 21:39:29 +0100

    Make nextjs compile

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-export default nextConfig;
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  
+  assetPrefix = `/${repo}`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'nate-925955698.imgix.net'
+  }
+}
+
